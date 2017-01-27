19f74852a4c44024ed12ec5c96452fe48cf98c9d
Richard Cook <rcook@rcook.org>
2017-01-26 21:34:01 -0800

    Create empty PandocBuildTool.Instances and Types modules

--- app/PandocBuildTool.hs
+++ app/PandocBuildTool.hs
@@ -1,5 +1,9 @@
 module PandocBuildTool
-    ( module PandocBuildTool.Render
+    ( module PandocBuildTool.Instances
+    , module PandocBuildTool.Render
+    , module PandocBuildTool.Types
     ) where
 
+import           PandocBuildTool.Instances
 import           PandocBuildTool.Render
+import           PandocBuildTool.Types
