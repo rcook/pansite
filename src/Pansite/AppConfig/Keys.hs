9d8caa794ee13f3e0117f7fd71481c54ed4e3d43
Richard Cook <rcook@rcook.org>
2017-01-30 08:28:41 -0800

    Remove first-name and last-name

--- src/Pansite/AppConfig/Keys.hs
+++ src/Pansite/AppConfig/Keys.hs
@@ -2,17 +2,9 @@
 
 module Pansite.AppConfig.Keys
     ( buildToolSettingsKey
-    , firstNameKey
-    , lastNameKey
     ) where
 
 import           Data.Text (Text)
-
-firstNameKey :: Text
-firstNameKey = "first-name"
-
-lastNameKey :: Text
-lastNameKey = "last-name"
 
 buildToolSettingsKey :: Text
 buildToolSettingsKey = "build-tool-settings"
