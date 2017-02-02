74bd7b40afc2960ce38a8a590d7105759e589a5b
Richard Cook <rcook@rcook.org>
2017-02-02 13:49:05 -0800

    Tidy up tools

--- app/PansiteApp/CopyTool.hs
+++ app/PansiteApp/CopyTool.hs
@@ -1,6 +1,6 @@
 {-|
 Module      : PansiteApp.CopyTool
-Description : Umbrella module for copy tool
+Description : Copy tool
 Copyright   : (C) Richard Cook, 2017
 Licence     : MIT
 Maintainer  : rcook@rcook.org
@@ -8,10 +8,26 @@ Stability   : experimental
 Portability : portable
 -}
 
-module PansiteApp.CopyTool
-    ( module PansiteApp.CopyTool.Funcs
-    , module PansiteApp.CopyTool.Types
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
 
-import           PansiteApp.CopyTool.Funcs
-import           PansiteApp.CopyTool.Types
+module PansiteApp.CopyTool (copyToolSpec) where
+
+import           Data.Aeson
+import           Data.Default
+import           Data.Yaml
+import           Pansite
+
+data CopySettings = CopySettings
+
+instance Default CopySettings where
+    def = CopySettings
+
+updater :: ParserContext -> CopySettings -> Value -> Parser CopySettings
+updater _ orig =
+    withObject "copy" $ \o -> pure orig
+
+runner :: ToolContext -> CopySettings -> IO ()
+runner _ _ = error "Not implemented"
+
+copyToolSpec :: ToolSpec
+copyToolSpec = ToolSpec "copy" updater runner
--- app/PansiteApp/PandocTool.hs
+++ app/PansiteApp/PandocTool.hs
@@ -1,6 +1,6 @@
 {-|
 Module      : PansiteApp.PandocTool
-Description : Umbrella module for copy tool
+Description : Pandoc tool
 Copyright   : (C) Richard Cook, 2017
 Licence     : MIT
 Maintainer  : rcook@rcook.org
@@ -8,10 +8,71 @@ Stability   : experimental
 Portability : portable
 -}
 
-module PansiteApp.PandocTool
-    ( module PansiteApp.PandocTool.Funcs
-    , module PansiteApp.PandocTool.Types
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
 
-import           PansiteApp.PandocTool.Funcs
-import           PansiteApp.PandocTool.Types
+module PansiteApp.PandocTool (pandocToolSpec) where
+
+import           Control.Monad
+import           Data.Aeson
+import           Data.Default
+import           Data.Yaml
+import           Pansite
+import           PansiteApp.Util
+import           Text.Blaze.Html.Renderer.String
+import           Text.Pandoc
+import           Text.Pandoc.XML
+
+data PandocSettings = PandocSettings
+    { psNumberSections :: Bool
+    , psVars :: [(String, String)]
+    , psTemplatePath :: Maybe FilePath
+    }
+
+instance Default PandocSettings where
+    def = PandocSettings False [] Nothing
+
+updater :: ParserContext -> PandocSettings -> Value -> Parser PandocSettings
+updater
+    (ParserContext resolveFilePath)
+    (PandocSettings numberSectionsOrig varsOrig mbTemplatePathOrig) =
+    withObject "pandoc" $ \o -> do
+        numberSections <- o .:? "number-sections" .!= numberSectionsOrig
+        vars <- o .:? "vars" .!= varsOrig
+        mbTemplatePathTemp <- o .:? "template-path" .!= mbTemplatePathOrig
+        let mbTemplatePath = resolveFilePath <$> mbTemplatePathTemp
+        return $ PandocSettings numberSections vars mbTemplatePath
+
+runner :: ToolContext -> PandocSettings -> IO ()
+runner
+    (ToolContext outputPath inputPaths _)
+    (PandocSettings numberSections vars mbTemplatePath) = do
+
+    putStrLn "PandocTool"
+    putStrLn $ "  outputPath=" ++ outputPath
+    putStrLn $ "  inputPaths=" ++ show inputPaths
+    putStrLn $ "  numberSections=" ++ show numberSections
+    putStrLn $ "  vars=" ++ show vars
+    putStrLn $ "  mbTemplatePath=" ++ show mbTemplatePath
+
+    when -- TODO: Should support multiple inputs
+        (length inputPaths /= 1)
+        (error "Pandoc tool currently supports exactly one input")
+
+    input <- readFileUtf8 (head inputPaths) -- TODO: Should support multiple inputs
+
+    mbTemplate <- case mbTemplatePath of
+                    Nothing -> return Nothing
+                    Just templatePath -> Just <$> readFileUtf8 templatePath
+
+    let Right doc = readMarkdown def input -- TODO: Irrefutable pattern
+        writerOpts = def
+            { writerTemplate = mbTemplate
+            , writerVariables = vars
+            , writerNumberSections = numberSections
+            }
+
+    let html = toEntities (renderHtml (writeHtml writerOpts doc))
+    writeFileUtf8 outputPath html
+
+pandocToolSpec :: ToolSpec
+pandocToolSpec = ToolSpec "pandoc" updater runner
