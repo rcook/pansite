c7a775bb84fb6e47a44908e3a6fb088133e0a22d
Richard Cook <rcook@rcook.org>
2017-02-15 20:27:29 -0800

    Set header-includes, include-before, include-after etc.

--- app/PansiteApp/PandocTool.hs
+++ app/PansiteApp/PandocTool.hs
@@ -32,29 +32,53 @@ data PandocSettings = PandocSettings
     , psTableOfContents :: Bool
     , psReferenceDocx :: Maybe FilePath
     , psMathJaxEnabled :: Bool
+    , psIncludeInHeaderPath :: Maybe FilePath
+    , psIncludeBeforeBodyPath :: Maybe FilePath
+    , psIncludeAfterBodyPath :: Maybe FilePath
     }
 
 instance Default PandocSettings where
-    def = PandocSettings False [] Nothing False Nothing False
+    def = PandocSettings False [] Nothing False Nothing False Nothing Nothing Nothing
 
 updater :: ParserContext -> PandocSettings -> Value -> Parser PandocSettings
 updater (ParserContext resolveFilePath) PandocSettings{..} =
-    withObject "pandoc" $ \o -> PandocSettings
-        <$> o .:? "number-sections" .!= psNumberSections
-        <*> o .:? "vars" .!= psVars
-        <*> fmap (resolveFilePath <$>) (o .:? "template-path" .!= psTemplatePath)
-        <*> o .:? "table-of-contents" .!= psTableOfContents
-        <*> fmap (resolveFilePath <$>) (o .:? "reference-docx" .!= psReferenceDocx)
-        <*> o .:? "mathjax" .!= psMathJaxEnabled
+    withObject "pandoc" $ \o ->
+        let getFilePath key def = fmap (resolveFilePath <$>) (o .:? key .!= def)
+        in PandocSettings
+            <$> o .:? "number-sections" .!= psNumberSections
+            <*> o .:? "vars" .!= psVars
+            <*> getFilePath "template-path" psTemplatePath
+            <*> o .:? "table-of-contents" .!= psTableOfContents
+            <*> getFilePath "reference-docx" psReferenceDocx
+            <*> o .:? "mathjax" .!= psMathJaxEnabled
+            <*> getFilePath "include-in-header-path" psIncludeInHeaderPath
+            <*> getFilePath "include-before-body-path" psIncludeBeforeBodyPath
+            <*> getFilePath "include-after-body-path" psIncludeAfterBodyPath
 
 mathJaxUrl :: String
 mathJaxUrl = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_CHTML-full"
 
+maybeReadFileUtf8 :: Maybe FilePath -> IO (Maybe String)
+maybeReadFileUtf8 (Just path) = Just <$> readFileUtf8 path
+maybeReadFileUtf8 Nothing = return Nothing
+
 mkWriterOptions :: PandocSettings -> IO WriterOptions
 mkWriterOptions PandocSettings{..} = do
-    mbTemplate <- case psTemplatePath of
-                    Nothing -> return Nothing
-                    Just templatePath -> Just <$> readFileUtf8 templatePath
+    mbTemplate <- maybeReadFileUtf8 psTemplatePath
+    mbIncludeInHeader <- maybeReadFileUtf8 psIncludeInHeaderPath
+    mbIncludeBeforeBody <- maybeReadFileUtf8 psIncludeBeforeBodyPath
+    mbIncludeAfterBody <- maybeReadFileUtf8 psIncludeAfterBodyPath
+
+    -- TODO: Let's do this more elegantly! Looks a little like a fold...
+    let psVars1 = case mbIncludeInHeader of
+                    Nothing -> psVars
+                    Just s -> ("header-includes", s) : psVars
+        psVars2 = case mbIncludeBeforeBody of
+                    Nothing -> psVars1
+                    Just s -> ("include-before", s) : psVars1
+        psVars3 = case mbIncludeAfterBody of
+                    Nothing -> psVars2
+                    Just s -> ("include-after", s) : psVars2
 
     let htmlMathMethod = if psMathJaxEnabled
                             then MathJax mathJaxUrl
@@ -66,7 +90,7 @@ mkWriterOptions PandocSettings{..} = do
         , writerTemplate = mbTemplate
         , writerTableOfContents = psTableOfContents
         , writerHTMLMathMethod = htmlMathMethod
-        , writerVariables = psVars
+        , writerVariables = psVars3
         }
 
 runner :: ToolContext -> PandocSettings -> IO ()
